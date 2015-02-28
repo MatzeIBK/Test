dcc515d26c3d801be810994a09af21196ec3629c
MatzeIBK <matthias.wanner@chello.at>
2015-02-28 12:24:11 +0100

    - faq

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 /* Timetable pressed */
 function swap(one, two) {
     var x = document.getElementsByClassName("container_Main");
-        document.getElementById(two).style.display = 'block';
-        document.getElementById(one).style.display = 'none';
+    x[0].document.getElementById(two).style.display = 'block';
+    x[1].document.getElementById(one).style.display = 'none';
 
 }
