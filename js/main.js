d39dbb426bf0d71532f2525051e04afb87684d44
MatzeIBK <matthias.wanner@chello.at>
2015-02-28 16:45:36 +0100

    ShowTimeTable function works

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 
 
 /* Timetable pressed */
-function swap(one, two) {
-    var x = document.getElementsByClassName("container_Main");
-    x[0].document.getElementById(two).style.display = 'block';
-    x[1].document.getElementById(one).style.display = 'none';
+function showTimeTable() {
+    var x = document.getElementById("main");
+    var y = document.getElementById("timeTable")
+    x.style.display = 'none';
+    y.style.display = 'block';
 
 }
