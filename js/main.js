1a7e0231473fbfb9dc17cf8f5fdff32b8357346e
MatzeIBK <matthias.wanner@chello.at>
2015-03-02 15:49:11 +0100

    last stand

--- js/main.js
+++ js/main.js
@@ -5,6 +5,12 @@ function showTimeTable() {
     var x = document.getElementById("main");
     var y = document.getElementById("timeTable")
     x.style.display = 'none';
-    y.style.display = 'block';
+    y.style.display = true;
+}
 
+/* back button pressed */
+
+function back_showHome() {
+    var x = document.getElementById("main");
+    x.style.display ="block";
 }
