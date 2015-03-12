a7ed74f886c1d53ce3ae00dbecf366f6152b8de0
MatzeIBK <matthias.wanner@chello.at>
2015-03-12 21:49:47 +0100

    dropdown test

--- js/main.js
+++ js/main.js
@@ -20,28 +20,15 @@ function back_showHome() {
 
 
 //html zu laden ist eher kontraproduktiov
-function load_faq(){
-    document.getElementById("main").innerHTML='<object type="text/html" data="faq.html"></object>';
+function load_faq() {
+    document.getElementById("main").innerHTML = '<object type="text/html" data="faq.html"></object>';
 }
 
-// get the width of the textarea minus scrollbar
 
 
-// width of our wrapper equals width of the inner part of the textarea
-
-
-
-
-
-
-
-
-
-
-// document ready!
-//mgölichkeit ein skelett zu füllen!
 
 $(function() {
+
     var $timetable = $('#tt_element');
 
 
@@ -53,32 +40,13 @@ $(function() {
     for(var i = 0; i < 10; i++) {
         //$main.append('<div class="wusler">'+ test[i].title +' / ' + test[i].author + '</div>')
 
-        $timetable.append('<div class="tt_e_left col-xs-3">'), $timetable.append('<div class="tt_e_right col-xs-9" id="tt_right">') ;
+        $timetable.append('<div class="tt_e_left col-xs-3">');
+        $timetable.append('<div class="tt_e_right col-xs-9" id="tt_right">') ;
     }
     $(".tt_e_right").append('<div class="tt_e_desc col-xs-12">');
     $(".tt_e_right").append('<div class="tt_e_low row" style="margin: 0px"> <div class="tt_e_stage col-xs-8"></div> <div class="tt_e_date col-xs-4"></div>');
+
 });
 
+$('#rightnow').click(showTimeTable());
 
-/* comment maybe used
-*
-*
-
- var test = [
- {
- title: 'hallo',
- author: 'wusler'
- }, {
- title: 'title',
- author: 'wusler1'
- }
- ];*
-*
-*
-*
-*
-*
-*
-*
-*
-* */
