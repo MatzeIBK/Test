65ed69d838b6995977f7f46321b7401f6009ebff
MatzeIBK <matthias.wanner@chello.at>
2015-03-13 14:33:47 +0100

    googlemaps online

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function showTimeTable() {
 
     hello.style.display = "none";
     timetable.style.display = "block";
-}
+};
 
 /* back button pressed */
 
@@ -16,29 +16,39 @@ function back_showHome() {
 
     hello.style.display = "block";
     timetable.style.display = "none";
-}
+};
 
 
 //html zu laden ist eher kontraproduktiov
 function load_faq() {
     document.getElementById("main").innerHTML = '<object type="text/html" data="faq.html"></object>';
-}
+};
 
 
+//document ready
+function googleLoad(){
+
+    var timetable = document.getElementById("first");
+    var temp = document.getElementById("second map_canvas");
+    timetable.style.display = "none";
+    temp.style.display = "block";
+    var mapOptions = {
+        center: new google.maps.LatLng(-34.397, 150.644),
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("second map_canvas"),
+        mapOptions);
+
+};
 
 
 $(function() {
 
     var $timetable = $('#tt_element');
-
-
     var $content = $('<div class="content">hallo</div>');
-
     $timetable.prepend($content);
-
-
     for(var i = 0; i < 10; i++) {
-        //$main.append('<div class="wusler">'+ test[i].title +' / ' + test[i].author + '</div>')
 
         $timetable.append('<div class="tt_e_left col-xs-3">');
         $timetable.append('<div class="tt_e_right col-xs-9" id="tt_right">') ;
@@ -46,7 +56,9 @@ $(function() {
     $(".tt_e_right").append('<div class="tt_e_desc col-xs-12">');
     $(".tt_e_right").append('<div class="tt_e_low row" style="margin: 0px"> <div class="tt_e_stage col-xs-8"></div> <div class="tt_e_date col-xs-4"></div>');
 
+
 });
 
 $('#rightnow').click(showTimeTable());
+$('#thursday').click(googleLoad());
 
