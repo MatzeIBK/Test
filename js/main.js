dc06017574d9a63da1f7c9cdafc063d28ed2b4f9
MatzeIBK <matthias.wanner@chello.at>
2015-03-13 14:43:23 +0100

    googlemaps online

--- js/main.js
+++ js/main.js
@@ -32,15 +32,56 @@ function googleLoad(){
     var temp = document.getElementById("second map_canvas");
     timetable.style.display = "none";
     temp.style.display = "block";
+
+    var map;
+
     var mapOptions = {
-        center: new google.maps.LatLng(-34.397, 150.644),
         zoom: 8,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById("second map_canvas"),
         mapOptions);
 
+// Try HTML5 geolocation
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = new google.maps.LatLng(position.coords.latitude,
+                position.coords.longitude);
+
+            var infowindow = new google.maps.InfoWindow({
+                map: map,
+                position: pos,
+                content: 'Location found using HTML5.'
+            });
+
+            map.setCenter(pos);
+        }, function() {
+            handleNoGeolocation(true);
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleNoGeolocation(false);
+    }
 };
+
+function handleNoGeolocation(errorFlag) {
+    if (errorFlag) {
+        var content = 'Error: The Geolocation service failed.';
+    } else {
+        var content = 'Error: Your browser doesn\'t support geolocation.';
+    }
+
+    var options = {
+        map: map,
+        position: new google.maps.LatLng(60, 105),
+        content: content
+    };
+
+    var infowindow = new google.maps.InfoWindow(options);
+    map.setCenter(options.position);
+}
+
+google.maps.event.addDomListener(window, 'load', googleLoad());
 
 
 $(function() {
