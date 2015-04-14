56ecd71beb538c728bb5e40b88086c7644493cec
MatzeIBK <matthias.wanner@chello.at>
2015-04-14 20:09:57 +0200

    slides

--- loadtest/loadtest2.js
+++ loadtest/loadtest2.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-    $("main2").css("display", "none");
-    $("main2").fadeIn(2000);
+    $("body").css("display", "none");
+    $("body").fadeIn(2000);
 
     $("a.transition").click(function (event) {
         event.preventDefault();
         linkLocation = this.href;
-        $("main2").fadeOut(1000, redirectPage);
+        $("body").fadeOut(1000, redirectPage);
     });
     function redirectPage() {
         window.location = linkLocation;
